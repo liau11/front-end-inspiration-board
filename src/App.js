796cbeac8b9973c7645a9c1089ad5fea0003992e
Lily <aulily09@gmail.com>
2023-06-26 14:48:28 -0700

    Co-authored-by: Lemmi (Niambi)<Lemmi-C@users.noreply.github.com> Create new board form and add new board data to sample data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import NewBoardForm from './components/NewBoardForm';
+
+
+const sampleData = [{ 'title': 'HIIIII', 'owner_name': 'Lily' }, { 'title': 'Byeee', 'owner_name': 'Niambi' }]
+
+const createNewBoard = (newBoard) => {
+  sampleData.push(newBoard)
+  console.log(sampleData)
+}
+
+
 
 function App() {
   return (
     <div className="App">
+      <NewBoardForm createNewBoard={createNewBoard} />
     </div>
   );
 }
--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,13 +1,57 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
 
 
-const NewBoardForm = () => {
+const DEFAULT_FORM = {
+    title: '',
+    owner_name: ''
+};
 
+const NewBoardForm = (props) => {
+    const [boardFormData, setBoardFormData] = useState(DEFAULT_FORM);
+
+    const handleChange = (event) => {
+        console.log(event.target.name)
+
+        const newFormData = {
+            ...boardFormData,
+            [event.target.name]: event.target.value
+        }
+        setBoardFormData(newFormData);
+    }
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        props.createNewBoard(boardFormData);
+        setBoardFormData(DEFAULT_FORM);
+    }
 
     return (
-        
+        <form onSubmit={handleFormSubmit}>
+            <label htmlFor="title">Title</label>
+            <input
+                type='text'
+                id='title'
+                name='title'
+                value={boardFormData.title}
+                onChange={handleChange}
+            />
+            <input type="submit" value="Add a title" />
+            <label htmlFor="owner_name">Owner's name</label>
+            <input
+                type='text'
+                id='owner_name'
+                name='owner_name'
+                value={boardFormData.owner_name}
+                onChange={handleChange}
+            />
+            <input type="submit" value="Add an owner" />
+        </form>
     )
 
 }
 
+NewBoardForm.propTypes = {
+    createNewBoard: PropTypes.func.isRequired,
+}
 
 export default NewBoardForm;
