dd40839a608ce609e344a2e3cd8f5e76103bebed
Lily <aulily09@gmail.com>
2023-06-27 11:50:38 -0700

    Display title of boards feature implemented

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import NewBoardForm from './components/NewBoardForm';
 import NewCardForm from './components/NewCardForm';
+import Board from './components/Board';
 
 
-const sampleData = [{ 'title': 'HIIIII', 'owner_name': 'Lily' }, { 'title': 'Byeee', 'owner_name': 'Niambi' }]
-const newSampleCard = [{ 'message': 'The bright red fox jumps over the fence.'}]
+const sampleBoardData = [{ 'title': 'HIIIII', 'owner_name': 'Lily' }, { 'title': 'Byeee', 'owner_name': 'Niambi' }]
+const newSampleCard = [{ 'message': 'The bright red fox jumps over the fence.' }]
 
 const createNewBoard = (newBoard) => {
-  sampleData.push(newBoard)
-  console.log(sampleData)
+  sampleBoardData.push(newBoard)
+  console.log(sampleBoardData)
 }
 
 const createNewCard = (newCard) => {
@@ -22,15 +23,17 @@ function App() {
   return (
     <div className="App">
       <h1>Inspiration Board</h1>
-        <h2>Create A New Board</h2>
-        <NewBoardForm createNewBoard={createNewBoard} />
-        <h2>Boards</h2>
-        <h2>Create A New Board</h2>
-        <div className="Card-Form">
-          <h2>Create a New Card</h2>
-          <NewCardForm createNewCard={createNewCard} />
-        </div>
+      <h2>Boards</h2>
+      <Board sampleBoardData={sampleBoardData} />
+      <h2>Create A New Board</h2>
+      <NewBoardForm createNewBoard={createNewBoard} />
+      <h2>Boards</h2>
+      <h2>Create A New Board</h2>
+      <div className="Card-Form">
+        <h2>Create a New Card</h2>
+        <NewCardForm createNewCard={createNewCard} />
       </div>
+    </div>
   );
 }
 
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,15 @@
 
 
-const Board = () => {
-
-
+const Board = ({ sampleBoardData }) => {
     return (
-        
+        <ol>
+            {sampleBoardData.map((board) => (
+                <li>{board.title}</li>
+            ))}
+        </ol>
     )
+};
 
-}
 
 
 export default Board;
