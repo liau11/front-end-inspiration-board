ab727a7657363b51a5ac23fb83f495d80920b985
Lily <aulily09@gmail.com>
2023-06-28 10:53:54 -0700

    Reinstate deleted changes from merging

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,13 +1,10 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
-import './NewBoardForm.css';
-
+import './NewBoardForm.js';
 
 const DEFAULT_FORM = {
     title: '',
-    owner_name: '',
-    preview: '', 
-    preview_2: ''
+    owner_name: ''
 };
 
 const NewBoardForm = (props) => {
@@ -39,18 +36,6 @@ const NewBoardForm = (props) => {
                 value={boardFormData.title}
                 onChange={handleChange}
             />
-            {/* <input type="submit" value="Add an owner" /> */}
-
-            <label htmlFor="preview">Preview:</label>
-            <input className="preview"
-                type='text'
-                id='message'
-                name='message'
-                value={boardFormData.title}
-                onChange={handleChange}
-            />
-
-            {/* <input type="submit" value="Add a title" /> */}
             <label htmlFor="owner_name">Owner's name</label>
             <input
                 type='text'
@@ -59,16 +44,15 @@ const NewBoardForm = (props) => {
                 value={boardFormData.owner_name}
                 onChange={handleChange}
             />
-            <label htmlFor="preview_2">Preview:</label> 
-            <input className="preview_2"
+            <input type="submit" value="Submit" />
+            <label htmlFor="preview">Preview Your Message Here</label>
+            <input className="preview"
                 type='text'
                 id='message'
                 name='message'
-                value={boardFormData.owner_name}
+                value={`${boardFormData.title}: ${boardFormData.owner_name}`}
                 onChange={handleChange}
             />
-            <input type="submit" value="submit" />
-
         </form>
 
     )
